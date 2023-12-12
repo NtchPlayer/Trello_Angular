52011b9b89de80bedd515c70aa6106cbfa0c7efd
Jérémie <barriere.jeremie@gmail.com>
2023-12-12 19:55:37 +0100

    [update] add/delete user and style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { ModalFolderComponent } from './shared/modal-folder/modal-folder.compone
 import { ModalTaskComponent } from './shared/modal-task/modal-task.component';
 import { CheckboxComponent } from './shared/checkbox/checkbox.component';
 import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
+import { TextareaComponent } from './shared/UI/textarea/textarea.component';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,9 @@ import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
     ModalFolderComponent,
     ModalTaskComponent,
     CheckboxComponent,
-    RegisterComponent
+    RegisterComponent,
+    TextareaComponent,
+    TextareaComponent
   ],
   imports: [
     CommonModule,
