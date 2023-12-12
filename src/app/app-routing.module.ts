46d2b002c00e2f53dc63181ef1081af5c54ca2ee
IcePlayer <nathanchevalet@orange.fr>
2023-12-12 19:13:24 +0100

    add home page add middleware

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,21 +4,44 @@ import { LoginComponent } from './view/login/login.component';
 import { FolderComponent } from './view/folder/folder.component';
 import { TaskComponent } from './view/task/task.component';
 import { RegisterComponent } from './view/register/register.component';
+import { HomeComponent } from './view/home/home.component';
+
+const AuthAccess = () => {
+  return Boolean(JSON.parse(localStorage.getItem('user')!))
+};
+
+const guestAccess = () => {
+  return !Boolean(JSON.parse(localStorage.getItem('user')!))
+};
 
 const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+  title: 'Home - EzTask',
+}, {
   path: 'login',
-  component: LoginComponent
+  component: LoginComponent,
+  title: 'Login - EzTask',
+  canActivate: [guestAccess],
 },{
   path: 'register',
-  component: RegisterComponent
+  component: RegisterComponent,
+  title: 'Register - EzTask',
+  canActivate: [guestAccess],
 }, {
   path: 'folder',
   component: FolderComponent,
+  title: 'Folder - EzTask',
+  canActivate: [AuthAccess],
 },  {
   path: 'folder/:id',
   component: TaskComponent,
-}
-]
+  title: 'Tasks - EzTask',
+  canActivate: [AuthAccess],
+}, {
+  path: '**',
+  redirectTo: '',
+}]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
